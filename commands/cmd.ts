bdbf6631c55e08ca830b72255e00497e228e3aba
Dylan Warren <duwtgb@mst.edu>
2018-07-23 02:42:03 -0500

    Man I hate TS sometimes

--- commands/cmd.ts
+++ commands/cmd.ts
@@ -1,9 +1,11 @@
 import ping from './ping'
 import dog from './dog'
+import leaderBoard from './leaderBoard'
 
 export default new Map<string, any>(
   [
     ['ping', ping],
     ['dog', dog],
+    ['leaderboard', leaderBoard]
   ]
 )
