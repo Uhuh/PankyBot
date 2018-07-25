51abf4c899eb07c58c222614a7cff328576795cd
Dylan Warren <duwtgb@mst.edu>
2018-07-25 18:54:56 -0500

    Umm git hello

--- events/log.ts
+++ events/log.ts
@@ -10,12 +10,8 @@ export default function log(client: PankyBot, member: GuildMember) {
     id: `${member.guild.id}-${member.id}`, 
     user: member.id, 
     guild: member.guild.id, 
-    date_active: `${((member.presence.status == 'online') 
-                      ? 
-                        moment().valueOf() : 
-                        (member.lastMessage 
-                      ? member.lastMessage.createdTimestamp :
-                        moment().valueOf()  ))}` 
+    date_active: `${((member.presence.status == 'online' || member.voiceChannel || !member.lastMessage)
+                      ? moment().valueOf() : member.lastMessage.createdTimestamp) }` 
   }
   client.setActivity.run(activity)
 }
