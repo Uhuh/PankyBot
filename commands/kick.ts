96754d861bd934aefa06699447fabd63f1b79bb1
Dylan Warren <duwtgb@mst.edu>
2018-07-26 19:55:23 -0500

    Better wording. Allow those with KICK_MEMBERS perm to run command not just admins.

--- commands/kick.ts
+++ commands/kick.ts
@@ -3,30 +3,30 @@ import { Message, Permissions, RichEmbed } from "discord.js";
 
 export default function kick(client: PankyBot, message: Message, args: string []) {
   let reason: string = ""
+  let name: string = ""
   let embed: RichEmbed = new RichEmbed()
-    .setAuthor(client.user.username, client.user.avatarURL)
+
   if (!message.guild) return
   // Don't let some crazy dude spam kick gotta make sure they're the OG
-  if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("You don't have permissions to use this command.")
+  if(!message.member.hasPermission("KICK_MEMBERS")) return message.reply("you don't have permissions to use this command.")
   // Skip all the mentions.
   for(const i of message.mentions.members) args.shift()
   // If there is a reason given
   for(const i of args) { reason += i + " " }
   // List who's being kicked and the reason if given.
   for(const [k, member] of message.mentions.members) {
-    
+    name = member.nickname?member.nickname:member.user.username
     member.kick(reason).then(() => {
       embed.setColor(65295)
-        .setTitle(`:wave: Bye ${member.nickname?member.nickname:member.user.username} :wave:`)
+        .setTitle(`:wave: Bye ${name} :wave:`)
         .setDescription('Successfully kicked user!')
         message.channel.send(embed)
     })
     .catch(() => {
       embed.setColor(16711683)
-        .setTitle(':octagonal_sign: **Unable to kick** :octagonal_sign:')
-        .setDescription('There was an issue while kicking')
-        .addField(':thinking:', 'I can\'t kick any role higher or equal to mine! Are you trying to kick someone above me?', true)
-        .addField(':thinking:', 'I might not have permissions to kick! Give me permissions by going to server settings and giving me a role that has the ability to kick!', true)
+        .setTitle(`:octagonal_sign: **Unable to kick ${name}** :octagonal_sign:`)
+        .addField('**__Check who you\'re trying to kick.__**', 'Discord doesn\'t allow a role to kick its equal or superior')
+        .addField('**__I might not have the correct permissions__**', `Panky should have admin perms when invited. Check this in your server settings.`)
         message.channel.send(embed)
       })
   }
