daf2978878a380962b384301ef2940d428cab267
Dylan Warren <duwtgb@mst.edu>
2018-10-10 11:51:03 -0500

    Allow users to look at info of others

--- commands/help/userinfo.ts
+++ commands/help/userinfo.ts
@@ -1,18 +1,23 @@
-import { Message } from "discord.js";
+import { Message, GuildMember } from "discord.js";
+import PankyBot from "../../src/bot";
 
 export default {
   desc: 'Information about the user; created account, roles, etc.',
   common: 'userinfo',
   args: '',
   alias: ['userinfo'],
-  run: async function (message: Message) {
+  run: async function (message: Message, args: string[], client: PankyBot) {
     let info: string = ""
-    info = `User: ${message.author.tag}`
-    info += `\nID: ${message.author.id}`
-    info += `\nName: ${message.author.username}`
-    info += `\nCreated: ${message.author.createdAt}`
-    if (message.author.presence.game) info += `\nPresence: ${message.author.presence.game.name}`
-    if (message.guild) info += `\nJoined: ${message.guild.members.get(message.author.id)!.joinedAt}`
+    const member: GuildMember | undefined = message.mentions.members.first().user.id !== client.user.id ? message.mentions.members.first() : undefined
+    const name = member ? ( member.nickname || member.user.username ) : ( message.member.nickname || message.author.username )
+    info = `User: ${member? member.user.tag : message.author.tag}`
+    info += `\nID: ${member? member.user.id : message.author.id}`
+    info += `\nName: ${name}`
+    info += `\nCreated: ${member ? member.user.createdAt : message.author.createdAt}`
+    if (member ? member.user.presence.game : message.author.presence.game) {
+      info += `\nPresence: ${member ? member.user.presence.game.name : message.author.presence.game.name}`
+    }
+    if (message.guild) info += `\nJoined: ${message.guild.members.get(member ? member.user.id : message.author.id)!.joinedAt}`
     message.channel.send(`\`\`\`ruby\n${info}\n\`\`\``)
   }
 }
