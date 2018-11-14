d8c037e55039844f28f92d61a2804bd0b5aa6322
Dylan Warren <duwtgb@mst.edu>
2018-11-14 10:27:05 -0600

    Let's not log so often

--- src/bot.ts
+++ src/bot.ts
@@ -43,7 +43,7 @@ export default class PankyBot extends Discord.Client {
   async start() {
     await this.login(this.config.TOKEN)
     activity(this)
-    // Log activity every 2.5minutes.
-    setInterval(() => activity(this), 150000) 
+    // Log activity every 10minutes.
+    setInterval(() => activity(this), 600000) 
   }
 }
