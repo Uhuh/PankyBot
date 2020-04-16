83bde34c0941b657a564187cc1c0d8d117a28e02
Dylan Warren <duwtgb@mst.edu>
2020-04-16 00:34:32 -0500

    stuff stuff

--- src/bot.ts
+++ src/bot.ts
@@ -1,7 +1,6 @@
 import * as Discord from 'discord.js'
 import * as dotenv from 'dotenv'
 dotenv.config()
-import * as DBL from 'dblapi.js'
 import msg from '../events/message'
 import * as config from './vars'
 import commandHandler from '../commands/commandHandler'
@@ -46,17 +45,28 @@ export default class PankyBot extends Discord.Client {
     commandHandler(this)
     
     // Discord bot list, gotta up them server numbers for certified )
-    this.dbl = new DBL(this.config.DBLTOKEN, this)
     this.on('ready', () => {
+      let baseIndex = 1;
+      const bases = [2, 8, 16, 32, 36];
       console.log(`[Started]: ${new Date()}`)
-      this.setInterval(() => this.dbl.postStats(this.guilds.cache.size), 1800000)
       this.setInterval(() => this.randPres(), 10000);
+      this.setInterval(() => {
+        const C_ID = "676613498968473610";
+        const G_ID = "647960154079232041";
+        const base = bases[baseIndex];
+        baseIndex === 4 ? (baseIndex=0) : baseIndex++;
+        const channel = this.guilds.cache.get(G_ID)?.channels.cache.get(C_ID) as Discord.TextChannel;
+        this.baseType = base;
+        channel.send(`Start counting in base ${base}!`)
+          .then(m => this.setTimeout(() => m.delete(), 600000));
+      }, 600000);
       this.memberCount();
     })
 
     this.on('message', (message: Discord.Message) => {
       if(message.channel.id === '676613498968473610') {
-        this.serverChannels(message);
+        if (message.author.bot) return;
+        this.count(message);
       } else {
         msg(this, message);
       }
@@ -122,10 +132,9 @@ export default class PankyBot extends Discord.Client {
     }).catch(console.error);
   }
 
-  serverChannels = (msg: Discord.Message) => {
+  count = (msg: Discord.Message) => {
     const G_ID = "647960154079232041";
     const MSG_VC = "676639231648464908";
-    const COUNT = "676613498968473610";
     const ROLE_ID = "677235204435476480";
     const MOD_ID = "647963820043534356";
     
@@ -134,6 +143,7 @@ export default class PankyBot extends Discord.Client {
     // How many points will said user get? :)
     // Always +1 by default
     let score = GET_SCORE.get(msg.author.id, G_ID);
+    console.log(score);
     if(!score) {
       console.log('score DNE?')
       score = { id: `${msg.author.id}-${G_ID}`, user: msg.author.id, guild: G_ID, points: 0 }
@@ -142,18 +152,17 @@ export default class PankyBot extends Discord.Client {
     if (!guild) return console.log("Somehow not in CS guild");
 
     const m_channel = guild.channels.cache.get(MSG_VC);
-    const count_channel = guild.channels.cache.get(COUNT) as Discord.TextChannel;
-
-    if(!m_channel || !count_channel) return console.log("VC Channel not avial");
+    if(!m_channel) return;
 
     const num = parseInt(msg.content.replace(/\s/g, ''), this.baseType);
+    console.log(`Num: ${num}`);
+
     if (Number.isNaN(num) || (
       (this.prevCount !== null && this.prevCount !== (num-1)) ||
       (this.prevCounter !== null && this.prevCounter === msg.author)
     )) {
       score.points--;
       // Take points away from losers.
-      console.log(`Taking away points... ${score.points}`);
       SET_SCORE.run(score);
       return msg.delete();
     }
@@ -173,9 +182,7 @@ export default class PankyBot extends Discord.Client {
       msg.member.roles.add(ROLE_ID);
     }
     
-    console.log(score);
     score.points++;
-    console.log(`Adding points.... ${score.points}`)
     SET_SCORE.run(score);
     m_channel.edit({
       name: `Count number: ${num}`
